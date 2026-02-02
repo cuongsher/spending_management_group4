d07b12a8b854fb9bf1e3ea02b8eae4120fe6344c
cuongsher <cuongsher@gmail.com>
2026-02-02 17:27:23 +0700

    Folder Features dùng để chứa các màn hình folder model dùng để chứa các file đối tượng

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:spending_management_group4/features/homePage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,85 +17,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 16),
-            // Tổng quan
-            Container(
-              height: 150,
-              color: Colors.pink,
-              padding: const EdgeInsets.all(12),
-              alignment: Alignment.topLeft,
-              child: const Text('Tổng quan'),
-            ),
-
-            const SizedBox(height: 16),
-            // Tình hình thu chi
-            Container(
-              height: 150,
-              color: Colors.red,
-              padding: const EdgeInsets.all(12),
-              alignment: Alignment.topLeft,
-              child: const Text('Tình hình thu chi'),
-            ),
-
-            const SizedBox(height: 16),
-            // Tổng quan lịch sử
-            Container(
-              height: 150,
-              color: Colors.blue,
-              padding: const EdgeInsets.all(12),
-              alignment: Alignment.topLeft,
-              child: const Text('Tổng quan lịch sử'),
-            ),
-
-            const SizedBox(height: 16),
-            // Hạn mức chi tiêu
-            Container(
-              height: 150,
-              color: Colors.yellow,
-              padding: const EdgeInsets.all(12),
-              alignment: Alignment.topLeft,
-              child: const Text('Hạn mức chi tiêu'),
-            ),
-
-            const SizedBox(height: 16),
-          ],
-        ),
-      ),
-
-      // Thanh điều hướng
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Trang chủ'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Báo cáo'),
-          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'Lịch sử'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Cá nhân'),
-        ],
-      ),
-    );
-  }
-}
